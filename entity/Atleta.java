4a869bcb49863a45c19871f92b7585385883026b
Guilherme Pongeti <gui22pongeti@gmail.com>
2020-09-21 22:52:23 +0000

    Test 1.4/ Vai dar boa

---
+++ entity/Atleta.java
@@ -0,0 +1,23 @@
+class Atleta{
+
+ private String nome;
+ private int idade;
+
+ public void setNome(String no){
+   this.nome = no;
+ }
+
+ public String getNome(){
+   return this.nome;
+ }
+
+ public void setIdade(int id){
+   this.idade = id;
+ }
+
+ public int getIdade(){
+   return this.idade;
+ }
+ 
+
+}
